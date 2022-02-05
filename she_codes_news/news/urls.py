5513fd8443eb981bb3a8d26e271f90fc85f4a3c4
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-02-05 09:03:22 +0800

    creating a form

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -5,5 +5,6 @@ app_name = 'news'
 
 urlpatterns = [
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('add-story/', views.AddStoryView.as_view(), name='newStory')
 ]
