eaf96fd739d1c878ce676651ac9b747abdeeaf59
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-02-07 10:39:01 +0800

    step 9 finished

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,5 +1,9 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from django.views import generic 
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 from django.contrib.auth import get_user_model
 
@@ -9,3 +13,8 @@ class ProfileView(generic.DetailView):
     model = USER_MODEL
     template_name = 'users/profile.html'
     context_object_name = 'user'
+
+class CreateAccountView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/createAccount.html'
