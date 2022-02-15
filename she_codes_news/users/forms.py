2cc926f8251dc9ef3c11ee8d073a75fe878b0429
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-02-15 19:35:09 +0800

    Update forms.py

--- she_codes_news/users/forms.py
+++ she_codes_news/users/forms.py
@@ -1,18 +1,15 @@
-from socket import fromshare
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ["username", "email"]
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ["username", "email"]
